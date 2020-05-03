16456d9e661154a0eae7c011ef5ed390e1f5b650
Joe May <joemaybooking@gmail.com>
2020-05-03 16:53:51 -0700

    added Auth/ fixed models from cat to poem

--- poems/main_app/models.py
+++ poems/main_app/models.py
@@ -17,7 +17,7 @@ class Meta:
 
 class Photo(models.Model):
   url = models.CharField(max_length=200)
-  cat = models.ForeignKey(Poems, on_delete=models.CASCADE)
+  poems = models.ForeignKey(Poems, on_delete=models.CASCADE)
 
   def __str__(self):
-    return f"Photo for cat_id: {self.cat_id} @{self.url}"
+    return f"Photo for poem_id: {self.poem_id} @{self.url}"
--- poems/main_app/views.py
+++ poems/main_app/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Buddy</h1>')
+  return render('<h1>home page</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def index(request):
+  return render(request, 'index.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
