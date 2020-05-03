98cd844da63b0875c96151029034143d8ab6d397
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-05-03 15:07:03 -0700

    basic models formed

--- poems/main_app/models.py
+++ poems/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poems(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=100)
+  poem = models.TextField(max_length=250)
+#   genre = models.Field(dont know the model for genre)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  date = models.DateField('Posted Date')
+
+class Meta:
+  ordering = ['-date']
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  cat = models.ForeignKey(Poems, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for cat_id: {self.cat_id} @{self.url}"
