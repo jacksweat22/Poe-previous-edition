d2601936925023e90c77e735ae461ec1f116ffda
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-05-03 23:19:47 -0700

    basic crud working, writing comments next

--- poems/main_app/models.py
+++ poems/main_app/models.py
@@ -4,27 +4,33 @@ from datetime import date
 from django.contrib.auth.models import User
 
 
-class Poems(models.Model):
+class Poem(models.Model):
   title = models.CharField(max_length=100)
   author = models.CharField(max_length=100)
   poem = models.TextField(max_length=250)
 #   genre = models.Field(dont know the model for genre)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date = models.DateField('Posted Date')
+  # user = models.ForeignKey(User, on_delete=models.CASCADE)
+  # date = models.DateField('Posted Date')
+
+  def __str__(self):
+    return self.title
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'poem_id': self.id})  
 
 class Meta:
   ordering = ['-date']
 
 
-class Comments:
-  poem = model.TextField(max_length=200)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Comment(models.Model):
+  poem = models.TextField(max_length=200)
+  # user = models.ForeignKey(User, on_delete=models.CASCADE)
   text = models.TextField(max_length=250)
 
 
 class Photo(models.Model):
   url = models.CharField(max_length=200)
-  poems = models.ForeignKey(Poems, on_delete=models.CASCADE)
+  poems = models.ForeignKey(Poem, on_delete=models.CASCADE)
 
   def __str__(self):
     return f"Photo for poem_id: {self.poem_id} @{self.url}"
--- poems/main_app/views.py
+++ poems/main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Poem
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
-from .models import Poems
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Poem
 
 # Create your views here.
 # from django.http import HttpResponse
@@ -17,8 +19,9 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-def index(request):
-  return render(request, 'index.html')
+def poems_index(request):
+  poems = Poem.objects.all()
+  return render(request, 'poems/index.html', { 'poems': poems })
 
 def signup(request):
   error_message = ''
@@ -34,11 +37,20 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-class PoeCreate(CreateView):
-  model = Poems
-  fields = ['title', 'poem'] 
+
+def poems_detail(request, poem_id):
+  poem = Poem.objects.get(id=poem_id)
+  return render(request, 'poems/detail.html', { 'poem': poem })
 
 class PoeCreate(CreateView):
-  model = Poems
-  fields = ['title', 'poem']  
-  success_url = '/index/'
+  model = Poem
+  fields = ['title', 'poem']   #make author value default to the user that made the post
+  success_url = '/poems/'
+
+class PoeUpdate(UpdateView):
+  model = Poem
+  fields = ['title', 'poem']
+
+class PoeDelete(DeleteView):
+  model = Poem
+  success_url = '/poems/'  
