2b19e25f05ed9f3817109a9cad778cee8fc08e6b
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-05-03 18:47:48 -0700

    added 'make a post' to nav bar and half the create for a new post

--- poems/main_app/urls.py
+++ poems/main_app/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
  path('', views.home, name='home'),
+ path('home/', views.home, name='home'),
  path('about/', views.about, name ='about'),
  path('accounts/signup/', views.signup, name='signup'),
  path('index/', views.index, name ='index'),
+ path('index/create', views.PoeCreate.as_view(), name='poes_create'),
 ]
--- poems/main_app/views.py
+++ poems/main_app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from .models import Poems
 
 # Create your views here.
 # from django.http import HttpResponse
 
 # Define the home view
+# def home(request):
+#   return HttpResponse('<h1>home page</h1>')
+
 def home(request):
-  return render('<h1>home page</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -28,3 +33,12 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class PoeCreate(CreateView):
+  model = Poems
+  fields = ['title', 'poem'] 
+
+class PoeCreate(CreateView):
+  model = Poems
+  fields = ['title', 'poem']  
+  success_url = '/index/'
