46ead7a460481ea086e45881727bfaeec8074c4e
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-05-03 19:55:36 -0700

    comment model

--- poems/main_app/models.py
+++ poems/main_app/models.py
@@ -15,6 +15,13 @@ class Poems(models.Model):
 class Meta:
   ordering = ['-date']
 
+
+class Comments:
+  poem = model.TextField(max_length=200)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  text = models.TextField(max_length=250)
+
+
 class Photo(models.Model):
   url = models.CharField(max_length=200)
   poems = models.ForeignKey(Poems, on_delete=models.CASCADE)
