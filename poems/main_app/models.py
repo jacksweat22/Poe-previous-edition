abdb991acf0f39f52129ed4d6c3236147e6fa567
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-05-04 12:20:20 -0700

    trying to have comment button redirect to that detail page without hard coding

--- poems/main_app/models.py
+++ poems/main_app/models.py
@@ -20,13 +20,17 @@ class Poem(models.Model):
 class Meta:
   ordering = ['-date']
 
-
 class Comment(models.Model):
   # poem = models.TextField(max_length=200)
-  # user = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   poem = models.ForeignKey(Poem, on_delete=models.CASCADE)
   text = models.TextField(max_length=10000)
 
+  def __str__(self):
+    return self.text
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'poem_id': self.id}) 
 
 class Photo(models.Model):
   url = models.CharField(max_length=200)
